fac60d1bb88cfa91f528038fafb7ca5cf147e617
Alexander Barth <barth.alexander@gmail.com>
2023-06-12 13:14:19 +0200

    visualization

---
+++ src/nc2leafletvelocity.jl
@@ -0,0 +1,141 @@
+using Dates
+using Interpolations
+using JSON3
+using NCDatasets
+
+function dd(lon,lat,u,parameterNumberName,parameterNumber,refTime)
+    return Dict{String,Any}(
+        "header" => Dict{String,Any}(
+            "la1"                 => lat[1],
+#            "la2"                 => lat[end],
+            "lo1"                 => lon[1],
+#            "lo2"                 => lon[end],
+            "dx"                  => lon[2,2] - lon[1,1],
+            "dy"                  => -(lat[2,2] - lat[1,1]),
+            "parameterNumber"     => parameterNumber,
+            "parameterNumberName" => parameterNumberName,
+            "parameterCategory"   => 2,
+            "refTime"             => refTime,
+            "nx"                  => size(u,1),
+            "ny"                  => size(u,2),
+            "parameterUnit"       => "m.s-1",
+        ),
+        "data" => vec(replace(u,NaN => 0))
+    )
+end
+
+
+
+function nc2json(lon,lat,time,u,v,f::IO; reduce = 1)
+#    @assert all(lon[1,:] .== lon[1,1])
+#    @assert all(lat[:,1] .== lat[1,1])
+
+    i = 1:reduce:size(u,1)
+    j = 1:reduce:size(u,2)
+
+    lon = lon[i,j]
+    lat = lat[i,j]
+    u = u[i,j]
+    v = v[i,j]
+
+    @show size(u)
+    refTime = Dates.format(time,"yyyy-mm-dd HH:MM:SS")
+    data = [dd(lon,lat,u,"Eastward current",2,refTime), dd(lon,lat,v,"Northward current",3,refTime)]
+
+
+    JSON3.write(f, data)
+end
+
+function nc2json(lon,lat,time,u,v,outfname::AbstractString; reduce = 1)
+    open(outfname,"w") do f
+        nc2json(lon,lat,time,u,v,f; reduce = reduce)
+    end
+end
+
+function nc2json(fname::AbstractString,varname,gridname,outfname; reduce = 1, ndepth = 1, ntime = 1)
+
+    ds = Dataset(fname);
+
+    ncu = ds[varname[1]]
+    ncv = ds[varname[2]]
+
+    lon_u = nomissing(NCDatasets.coord(ncu,"longitude")[:,:])
+    lat_u = nomissing(NCDatasets.coord(ncu,"latitude")[:,:])
+
+    lon_v = nomissing(NCDatasets.coord(ncv,"longitude")[:,:])
+    lat_v = nomissing(NCDatasets.coord(ncv,"latitude")[:,:])
+
+    time = NCDatasets.coord(ncu,"time")[ntime]
+    @show time
+
+    lon = nomissing(ds[gridname[1]][:,:])
+    lat = nomissing(ds[gridname[2]][:,:])
+
+    itp_u = LinearInterpolation((lon_u[:,1],lat_u[1,:]),nomissing(ncu[:,:,ndepth,ntime],NaN),extrapolation_bc = NaN);
+    itp_v = LinearInterpolation((lon_v[:,1],lat_v[1,:]),nomissing(ncv[:,:,ndepth,ntime],NaN),extrapolation_bc = NaN);
+
+    u = itp_u.(lon,lat);
+    v = itp_v.(lon,lat);
+
+
+    nc2json(lon,lat,time,u,v,outfname; reduce = reduce)
+
+    close(ds)
+end
+
+#=
+
+fname = "/home/abarth/tmp/champs_corse_BE201706_surface_20170601.nc"
+outfname = "/home/abarth/src/leaflet-velocity/demo2/model-MARS.json"
+varname = ("UZ","VZ")
+gridname = ("longitude","latitude")
+ndepth = 1
+ntime = 1
+
+
+
+nc2json(fname,varname,gridname,outfname;
+        ndepth = ndepth,
+        ntime = ntime
+)
+
+fname = "/mnt/data1/abarth/work/LS2v/Calvi-2015-u3hadv-vega/Calvi-2015-u3hadv/ocean_his.nc"
+outfname = "/home/abarth/src/leaflet-velocity/demo2/model-ROMS.json"
+varname = ("u","v")
+gridname = ("lon_rho","lat_rho")
+ndepth = 32
+ntime = 3
+
+nc2json(fname,varname,gridname,outfname;
+        ndepth = ndepth,
+        ntime = ntime
+)
+
+
+fname = "/mnt/data1/abarth/work/LS2v/Calvi-2015-u3hadv-vega/Calvi-2015-u3hadv/ocean_nest_his.nc"
+outfname = "/home/abarth/src/leaflet-velocity/demo2/model-ROMS-nest.json"
+varname = ("u","v")
+gridname = ("lon_rho","lat_rho")
+ndepth = 32
+ntime = 3
+
+nc2json(fname,varname,gridname,outfname;
+        ndepth = ndepth,
+        ntime = ntime
+)
+
+
+
+fname = "/mnt/data1/abarth/work/LS2v/Calvi-2015-u3hadv-vega/Calvi-2015-u3hadv/ocean_nest_calvi_his.nc"
+outfname = "/home/abarth/src/leaflet-velocity/demo2/model-ROMS-nest_calvi.json"
+varname = ("u","v")
+gridname = ("lon_rho","lat_rho")
+ndepth = 32
+ntime = 3
+
+nc2json(fname,varname,gridname,outfname;
+        ndepth = ndepth,
+        ntime = ntime
+)
+
+=#
---
+++ src/plotting.jl
@@ -0,0 +1,107 @@
+module Plotting
+using Random
+
+include("nc2leafletvelocity.jl")
+
+
+const leaflet_init = Ref(false);
+
+function setup()
+    if leaflet_init[]
+        return
+    end
+    display("text/javascript", """
+var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://data-assimilation.net/upload/Alex/leaflet-velocity/demo2/leaflet.js';
+    document.head.appendChild(script)
+""")
+
+
+    display("text/javascript", """
+var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://data-assimilation.net/upload/Alex/leaflet-velocity/dist/leaflet-velocity.min.js';
+    document.head.appendChild(script)
+""")
+
+    display("text/html","""<link rel="stylesheet" href="https://npmcdn.com/leaflet@1.1.0/dist/leaflet.css" />
+<link rel="stylesheet" href="http://data-assimilation.net/upload/Alex/leaflet-velocity/dist/leaflet-velocity.min.css" />
+<link rel="stylesheet" href="http://data-assimilation.net/upload/Alex/leaflet-velocity/demo2/demo.css" />
+""");
+    leaflet_init[] = true;
+end
+
+
+function plot(lon,lat,u,v;
+              reduce = 1,
+              time = DateTime(2000,1,1),
+              scale = 10,
+              zoom = 5,
+              center = (41,9),
+              maxvelocity = 0.2,
+              )
+
+    setup();
+    io = IOBuffer()
+    nc2json(lon,lat,time,u,v,io; reduce = 1)
+    str = String(take!(io))
+    datastr = "data" * randstring(10);
+    mapid = "map" * randstring(10);
+
+    display("text/javascript","""window.$(datastr) = JSON.parse(`$(str)`);""");
+
+    display("text/html","""<div id="$mapid" style="width: 800px; height: 400px;"></div>""")
+    display("text/javascript","""
+ var Esri_WorldImagery = L.tileLayer(
+    "http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+    {
+      attribution:
+        "BlueCloud; Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, " +
+        "AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
+    }
+  );
+
+  var Esri_DarkGreyCanvas = L.tileLayer(
+    "http://{s}.sm.mapstack.stamen.com/" +
+      "(toner-lite,\$fff[difference],\$fff[@23],\$fff[hsl-saturation@20])/" +
+      "{z}/{x}/{y}.png",
+    {
+      attribution:
+        "BlueCloud; Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, " +
+        "NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community"
+    }
+  );
+
+  var baseLayers = {
+    "Satellite": Esri_WorldImagery,
+    "Grey Canvas": Esri_DarkGreyCanvas
+  };
+
+  var map = L.map("$mapid", {
+    layers: [Esri_WorldImagery],
+    scrollWheelZoom: false
+  });
+
+  var layerControl = L.control.layers(baseLayers);
+  layerControl.addTo(map);
+  map.setView([$(center[1]), $(center[2])], $(zoom));
+
+  var velocityLayer = L.velocityLayer({
+            displayValues: true,
+            displayOptions: {
+                velocityType: "GBR Water",
+                displayPosition: "bottomleft",
+                displayEmptyString: "No water data"
+            },
+            data: $(datastr),
+            maxVelocity: $(maxvelocity),
+            velocityScale: $(scale) // arbitrary default 0.005
+        });
+
+  layerControl.addOverlay(velocityLayer, name);
+  velocityLayer.addTo(map);
+""");
+end
+
+end
