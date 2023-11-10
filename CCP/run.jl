69b9d4a5aa091fa9b7f2cfca83852c0d99da7667
Alexander Barth <barth.alexander@gmail.com>
2023-11-10 08:18:39 +0100

    add other scripts

--- CCP/run.jl
+++ CCP/run.jl
@@ -4,3 +4,7 @@ using CoastalCurrents
 pathname = joinpath(dirname(pathof(CoastalCurrents)),"..","examples")
 
 include(joinpath(pathname,"prep_altimetry.jl"))
+include(joinpath(pathname,"prep_drifter.jl"))
+
+include(joinpath(pathname,"currents_altimetry.jl"))
+include(joinpath(pathname,"currents_drifter.jl"))
