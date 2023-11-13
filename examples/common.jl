f4ada2753417a91ff94faa25d13e7c2f4223251d
Alexander Barth <barth.alexander@gmail.com>
2023-11-13 13:27:22 +0000

    clean-up

--- examples/common.jl
+++ examples/common.jl
@@ -2,23 +2,13 @@ using Downloads: download
 
 varname = "sla"
 
-
-lonr = [-7,37]
-latr = [30,46]
-
-
 dlon = dlat = 0.25
-
-dlon = dlat = 0.25
-
 lonr = -7:dlon:37
 latr = 30:dlat:46
 
 product_id = "SEALEVEL_EUR_PHY_L3_MY_008_061"
 
-
 basedir = expanduser("~/tmp/BlueCloud2026")
-
 
 altimetry_dir = joinpath(basedir,"Altimetry")
 drifter_dir = joinpath(basedir,"Drifter")
